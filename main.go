8a3a4dd20435efb6dd65fa1ba783ca17b9885bc6
neil605164 <neil605164work@gmail.com>
2018-08-04 17:18:55 +0800

    [新增] Interface 觀念

--- main.go
+++ main.go
@@ -2,6 +2,30 @@ package main
 
 import "fmt"
 
+type myName struct {
+	Username string `json:"username"`
+}
+
+func (c myName) getMe(string) (name string) {
+	name = "Neil"
+	return
+}
+
 func main() {
-	fmt.Println("Hello World")
+	var data interface {
+		getMe(string) string
+	}
+	fmt.Println("data ---->", data)
+
+	// 噴錯誤，原因是沒有getMe func
+	data = 1
+	fmt.Println("data ---->", data)
+
+	// 噴錯誤，原因是沒有getMe func
+	data = "OK"
+	fmt.Println("data ---->", data)
+
+	// 正確，因為myName這一個物件，有包含getMe的func
+	data = myName{}
+	fmt.Println("data ---->", data)
 }
