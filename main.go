11a7ca9e8d0b2683a34dade4c71aaa062b679db4
neil605164 <neil605164work@gmail.com>
2018-08-04 16:49:13 +0800

    [新增] 將struct 視為class 的概念

--- main.go
+++ main.go
@@ -13,11 +13,13 @@ func main() {
 	r.Run()
 }
 
+// class 概念
 type jsonData struct {
-	Status  string      `json:"status"`
-	Message string      `json:"message"`
-	Data    interface{} `json:"data"`
-	NotUse  int         `json:"-"`
+	Username string      `json:"username"`
+	Status   string      `json:"status"`
+	Message  string      `json:"message"`
+	Data     interface{} `json:"data"`
+	NotUse   int         `json:"-"`
 }
 
 type obj struct {
@@ -25,10 +27,16 @@ type obj struct {
 	Age  int    `json:"age"`
 }
 
+func (c *jsonData) getMe() (myname string) {
+	myname = "My name is: " + c.Username
+	return
+}
+
 func getData(c *gin.Context) {
-	jsonData := jsonData{
-		Status:  "OK",
-		Message: "Access",
+	data := jsonData{
+		Username: "Neil",
+		Status:   "OK",
+		Message:  "Access",
 		Data: []obj{
 			obj{Name: "Neil", Age: 25},
 			{Name: "Neil1", Age: 25},
@@ -36,5 +44,7 @@ func getData(c *gin.Context) {
 		},
 		NotUse: 1,
 	}
-	c.JSON(200, jsonData)
+
+	myname := data.getMe()
+	c.JSON(200, myname)
 }
