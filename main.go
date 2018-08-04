915622fb141667c0b72676ebd3b9701714fbacc4
neil605164 <neil605164work@gmail.com>
2018-08-04 18:25:39 +0800

    [新增] （非同步）挖掘礦坑

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"time"
 )
@@ -9,50 +8,90 @@ import (
 func main() {
 	// 一片土地
 	theMine := []string{"rock", "ore", "rock", "ore", "rock"}
+	// 宣告搜尋礦坑的通道
+	foundOerChannel := make(chan string)
+	// 宣告找到礦石的通道
+	minedOerChannel := make(chan string)
 
 	// 整理出一堆礦坑
-	foundOre := finder(theMine)
-	fmt.Println("找到礦物 ---->", foundOre)
+	// foundOre := finder(theMine)
+	// fmt.Println("找到礦物 ---->", foundOre)
+	go finder(theMine, foundOerChannel)
 
 	// 整理出一堆礦物
-	minerOre := miner(foundOre)
-	fmt.Println("挖好礦物 ---->", minerOre)
+	// minerOre := miner(foundOre)
+	// fmt.Println("挖好礦物 ---->", minerOre)
+	go miner(foundOerChannel, minedOerChannel)
 
 	// 提煉成黃金
-	gold := smelter(minerOre)
-	fmt.Println("提煉好礦物 ---->", gold)
+	// gold := smelter(minerOre)
+	// fmt.Println("提煉好礦物 ---->", gold)
+	smelter(minedOerChannel)
 }
 
 // 找礦物者
-func finder(mines []string) (oreMines []string) {
+func finder(mines []string, foundOerChannel chan string) {
 	for i, mine := range mines {
 		if mine == "ore" {
 			log.Println("開始找礦坑")
-			oreMines = append(oreMines, mine)
+			// oreMines = append(oreMines, mine)
+			foundOerChannel <- mine
 			log.Println("找到第", i, "礦坑")
 		}
 	}
+
+	// 當沒有礦區要尋找礦場時，主動關閉通道
+	close(foundOerChannel)
 	return
 }
 
 // 挖礦物者
-func miner(mines []string) (oreMines []string) {
-	for _, mine := range mines {
-		log.Println("開始挖掘一個礦石")
+func miner(foundOerChannel, minedOerChannel chan string) {
+	i := 0
+	for {
+		mine := <-foundOerChannel
+		// 判斷是否還有礦區要挖礦
+		if mine != "ore" {
+
+			// 當沒有礦要挖掘時，主動關閉通道
+			close(minedOerChannel)
+			break
+		}
+		log.Println("開始挖掘第", i+1, "顆礦石")
 		time.Sleep(time.Second)
-		oreMines = append(oreMines, mine)
-		log.Println("挖好一顆礦石")
+
+		minedOerChannel <- mine
+		log.Println("挖好第", i+1, "顆礦石")
+		i++
 	}
-	return
+	// for _, mine := range mines {
+	// 	log.Println("開始挖掘一個礦石")
+	// 	time.Sleep(time.Second)
+	// 	oreMines = append(oreMines, mine)
+	// 	log.Println("挖好一顆礦石")
+	// }
+	// return
 }
 
 // 煉金人員
-func smelter(mines []string) (gold []string) {
-	for i, mine := range mines {
+func smelter(minedOerChannel chan string) {
+	i := 0
+	for {
+		mine := <-minedOerChannel
+		// 判斷是否還有礦要提煉
+		if mine != "ore" {
+			break
+		}
 		log.Println("開始提煉第", i+1, "顆礦物")
 		time.Sleep(time.Second)
-		gold = append(gold, mine)
 		log.Println("提煉完成第", i+1, "顆礦物")
+		i++
 	}
-	return
+	// for i, mine := range mines {
+	// 	log.Println("開始提煉第", i+1, "顆礦物")
+	// 	time.Sleep(time.Second)
+	// 	gold = append(gold, mine)
+	// 	log.Println("提煉完成第", i+1, "顆礦物")
+	// }
+	// return
 }
