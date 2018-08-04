7144551456a77ffbee0a6f46c321f89e6d7a961a
neil605164 <neil605164work@gmail.com>
2018-08-04 16:31:54 +0800

    [新增] Struct 概念

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
+// Struct 教學
+
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello World")
+
+	r := gin.Default()
+	r.GET("/", getData)
+	r.Run()
+}
+
+type jsonData struct {
+	Status  string      `json:"status"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+	NotUse  int         `json:"-"`
+}
+
+type obj struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func getData(c *gin.Context) {
+	jsonData := jsonData{
+		Status:  "OK",
+		Message: "Access",
+		Data: []obj{
+			obj{Name: "Neil", Age: 25},
+			{Name: "Neil1", Age: 25},
+			{Name: "Neil2", Age: 25},
+		},
+		NotUse: 1,
+	}
+	c.JSON(200, jsonData)
 }
