9900406549981e45430767d6f13baf6a8ab77f06
neil605164 <neil605164work@gmail.com>
2018-08-06 16:44:21 +0800

    [新增] 載入pkg練習

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"HTTP_GO/helloworld"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	helloworld.Helloworld()
+	// fmt.Println(helloworld.Helloworld())
 }
