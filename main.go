3327a82e323dbd268fcdd9a7abeb0557bace210d
neil605164 <neil605164work@gmail.com>
2018-08-07 17:14:38 +0800

    [新增] 字串函式練習

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println(
+		// 比對字串是否有符合條件
+		strings.Contains("test", "es"),
+
+		// 比較字串是否相符合
+		strings.Compare("A", "a"),
+
+		// 比較任何符號
+		strings.ContainsAny("I'm a boy", "'"),
+
+		// 字串相加
+		strings.Join([]string{"a", "b", "c"}, "-"),
+
+		// 重複印出指定字串
+		strings.Repeat("#", 5),
+
+		// 字串轉換成大寫
+		strings.ToLower("Fdwgb"),
+
+		// 字串轉換成小寫
+		strings.ToUpper("FredwC"),
+
+		// 計算某字串出現幾次
+		strings.Count("no no no no yes", "no"),
+	)
 }
