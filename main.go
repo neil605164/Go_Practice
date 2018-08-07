20417fec3ea85a61f8c9e6d07c0c7458a8ec2c4e
neil605164 <neil605164work@gmail.com>
2018-08-07 13:16:17 +0800

    [新增]讀檔練習

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	file, err := os.Open("text1.txt")
+	if err != nil {
+		fmt.Println("File is not exit")
+		return
+	}
+
+	defer file.Close()
+
+	stat, err := file.Stat()
+	if err != nil {
+		fmt.Println("Content is null")
+		return
+	}
+
+	bs := make([]byte, stat.Size())
+	fmt.Println(bs)
+	_, err = file.Read(bs)
+	if err != nil {
+		return
+	}
+
+	str := string(bs)
+	fmt.Println(str)
 }
