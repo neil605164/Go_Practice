2863fee0c4dc6239fad48a0acc684eaa14b203aa
neil605164 <neil605164work@gmail.com>
2018-08-07 14:55:17 +0800

    [新增] 時間練習

--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// Add 时间相加
+	now := time.Now()
+
+	// 1分钟前
+	m1 := now.Add(-time.Minute)
+	fmt.Println(m1)
+
+	// 8个小时前
+	h1 := now.Add(-time.Hour * 8)
+	fmt.Println(h1)
+
+	// 一天前
+	d1 := now.Add(-time.Hour * 24)
+	fmt.Println(d1)
+
+	printSplit(50)
+
+	// 10分钟后
+	m1 = now.Add(time.Minute)
+	fmt.Println(m1)
+
+	// 8小时后
+	h1 = now.Add(time.Hour * 8)
+	fmt.Println(h1)
+
+	// 一天后
+	d1 = now.Add(time.Hour * 24)
+	fmt.Println(d1)
+
+	printSplit(50)
+
+	// Sub 计算两个时间差
+	subM := now.Sub(m1)
+	fmt.Println(subM.Minutes(), "分钟")
+
+	sumH := now.Sub(h1)
+	fmt.Println(sumH.Hours(), "小时")
+
+	sumD := now.Sub(d1)
+	fmt.Printf("%v 天\n", sumD.Hours()/24)
+
+}
+
+func printSplit(count int) {
+	fmt.Println(strings.Repeat("#", count))
 }
