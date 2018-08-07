e4818b23232551b23c5678929dd76293cda13cf8
neil605164 <neil605164work@gmail.com>
2018-08-07 17:20:46 +0800

    [修改] 第二次string練習

--- main.go
+++ main.go
@@ -30,5 +30,11 @@ func main() {
 
 		// 計算某字串出現幾次
 		strings.Count("no no no no yes", "no"),
+
+		// 檢查字頭是否存在某字串
+		strings.HasPrefix("dgfyud_neil", "dgfyud_"),
+
+		// 檢查字尾是否存在某字串
+		strings.HasSuffix("neil_dgfyud", "_dgfyud"),
 	)
 }
