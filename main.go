4a267f07edb73278de037c78dc186a1131a33ec1
neil605164 <neil605164work@gmail.com>
2018-08-10 17:00:57 +0800

    [新增] curl取資料（post）

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
 
 func main() {
+	httpPost()
 	fmt.Println("Hello World")
+}
+
+func httpPost() {
+	url := "https://tw.yahoo.com/"
+
+	payload := strings.NewReader("test=neil")
+
+	req, _ := http.NewRequest("POST", url, payload)
+
+	req.Header.Add("content-type", "application/x-www-form-urlencoded")
+	req.Header.Add("cache-control", "no-cache")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(res)
+	fmt.Println(string(body))
 }
