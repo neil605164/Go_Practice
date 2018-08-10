b6e5454452a2b998528670d533a4d7e724b01bb7
neil605164 <neil605164work@gmail.com>
2018-08-10 14:30:02 +0800

    [新增] HTTP/GET 方式

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
+	httpGet()
 	fmt.Println("Hello World")
+}
+
+func httpGet() string {
+	resp, err := http.Get("https://tw.yahooooo.com/")
+	if err != nil {
+		return "HTTP Connect URL ERROR"
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "GET URL INFO ERROR"
+	}
+
+	fmt.Println(string(body))
+	return ""
 }
