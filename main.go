da32c50d2de629ade32115999687ec8bedfd7562
neil605164 <neil605164work@gmail.com>
2018-08-23 23:00:26 +0800

    [新增] Error 處理方式

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+func checkoutUserIsExist(username string) (bool, error) {
+	if username == "bar" {
+		return true, errors.New(fmt.Sprintf("The %s user is exist", username))
+	}
+
+	if username == "foo" {
+		return true, fmt.Errorf("The %s user is exist", username)
+	}
+	return false, nil
+}
 
 func main() {
 	fmt.Println("Hello World")
+
+	if _, err := checkoutUserIsExist("bar"); err != nil {
+		fmt.Println(err)
+	}
+
+	if _, err := checkoutUserIsExist("foo"); err != nil {
+		fmt.Println(err)
+	}
 }
