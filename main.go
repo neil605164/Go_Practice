d31d3912ff21e791a19718af8cdc7be34b7e6b60
neil605164 <neil605164work@gmail.com>
2018-08-26 16:31:22 +0800

    [ADD] Prac SwitchCase

--- main.go
+++ main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+func checkValue(num int) {
+	switch num {
+	case 0:
+		fallthrough // 若符合，繼續往下跑
+	case 1:
+		fmt.Printf("num is %d", num)
+		fmt.Println()
+	case 2, 3:
+		fmt.Println("值為：", num)
+	}
+}
+
 func main() {
-	fmt.Println("Hello World")
+	checkValue(0)
+	checkValue(1)
+	checkValue(2)
+	checkValue(3)
 }
