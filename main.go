32ab591a8437474ea17b2aea1bab7251559f584e
neil605164 <neil605164work@gmail.com>
2018-08-28 00:06:19 +0800

    [ADD] 效能測試練習

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
+
+// 數字轉字串
+func print01(num int) string {
+	return fmt.Sprintf("%d", num)
+}
+
+// 數字轉字串
+func print02(num int64) string {
+	return strconv.FormatInt(num, 10)
+}
+
+// 數字轉字串
+func print03(num int) string {
+	return strconv.Itoa(num)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	log.Println(print01(100))
+	log.Println(print02(100))
+	log.Println(print03(100))
 }
