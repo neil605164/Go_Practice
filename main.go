84245a05c00416705a6a0bbbd77c2548348d111f
neil605164 <neil605164work@gmail.com>
2018-08-30 21:11:08 +0800

    [ADD] Prac_goroutine

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
+	msg := "Let's Go"
+	// 印出結果為："Let's GOGOGO"
+	go func() {
+		fmt.Println("msg:", msg)
+	}()
+
+	// 印出結果為："Let's GO"
+	go func(input string) {
+		fmt.Println("input:", input)
+	}(msg)
+
+	msg = "Let's GOGOGO"
+	time.Sleep(1 * time.Second)
 	fmt.Println("Hello World")
 }
