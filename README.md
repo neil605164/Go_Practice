d4beda3b104c5790f515d662d223a82868d1b9b2
neil605164 <neil605164work@gmail.com>
2018-08-30 22:15:12 +0800

    [ADD] Prac Flag

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Go_Practice
+ - 指令
+    # 查看有哪些參數可以帶入
+    - go run main.go -h
+    # 啟動服務
+    - PORT=8083 go run main.go
+    - go run main.go -port1 8082
--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "I love %s!", r.URL.Path[1:])
+}
 
 func main() {
-	fmt.Println("Hello World")
+	var port string
+	flag.StringVar(&port, "port1", "8080", "server port")
+	flag.StringVar(&port, "port2", "8081", "server port")
+	flag.Parse()
+
+	if p, ok := os.LookupEnv("PORT"); ok {
+		port = p
+	}
+
+	http.HandleFunc("/", handler)
+	log.Println("http server run on ", port, " port")
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
