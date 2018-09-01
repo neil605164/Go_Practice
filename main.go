5b8487f2637739eaa2d0100f8561dc182af8779a
neil605164 <neil605164work@gmail.com>
2018-09-01 11:04:11 +0800

    [ADD] 練習build docker 容器，尚未完成

--- main.go
+++ main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Println("http request.")
+	fmt.Fprintf(w, "I love %s!", r.URL.Path[1:])
+}
+
+func pinger(port string) error {
+	resp, err := http.Get("http://localhost:" + port)
+
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("server return not 200 status code")
+	}
+
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello World")
+	var port string
+	var ping bool
+
+	flag.StringVar(&port, "port1", "8080", "server port")
+	flag.StringVar(&port, "port2", "8081", "server port")
+	flag.BoolVar(&ping, "ping", false, "check_docker_server")
+	flag.Parse()
+
+	// check http service
+	if p, ok := os.LookupEnv("PORT"); ok {
+		port = p
+	}
+
+	// check docker service
+	if ping {
+		if err := pinger(port); err != nil {
+			log.Printf("Ping docker server error: %v \n", err)
+		}
+
+		return
+	}
+
+	http.HandleFunc("/", handler)
+	log.Println("http server run on ", port, " port")
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
