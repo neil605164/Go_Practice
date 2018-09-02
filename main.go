36bda25d14cb59475cf988458c84030b69480575
neil605164 <neil605164work@gmail.com>
2018-09-02 13:34:33 +0800

    [ADD] 新增sync.Mutex luck 練習方式

--- main.go
+++ main.go
@@ -1,3 +1,7 @@
+// 啟動服務後，執行以下指令
+// go run main.go
+// echo "GET http://localhost:8000" | vegeta attack -rate=100 -connections=1 -duration=1s | tee result.bin | vegeta report
+
 package main
 
 import (
@@ -6,6 +10,7 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"sync"
 	"time"
 
 	"gopkg.in/mgo.v2"
@@ -13,7 +18,8 @@ import (
 )
 
 var globalDB *mgo.Database
-var account = "appleboy"
+var account = "neil_hsieh"
+var mu = &sync.Mutex{}
 
 type currency struct {
 	ID      bson.ObjectId `json:"id" bson:"_id,omitempty"`
@@ -31,6 +37,13 @@ func Random(min, max int) int {
 
 func pay(w http.ResponseWriter, r *http.Request) {
 	entry := currency{}
+
+	//將以下程式碼lock住，不讓其他請求先行執行
+	mu.Lock()
+	// 當執行完畢pay handler最後一行後，會在執行defer底下的條件
+	// 在此是unlock程式碼
+	defer mu.Unlock()
+
 	// step 1: get current amount
 	err := globalDB.C("bank").Find(bson.M{"account": account}).One(&entry)
 
@@ -42,7 +55,7 @@ func pay(w http.ResponseWriter, r *http.Request) {
 	time.Sleep(time.Duration(wait) * time.Millisecond)
 
 	//step 3: subtract current balance and update back to database
-	entry.Amount = entry.Amount + 50.000
+	entry.Amount = entry.Amount + 50.000 // 預計取的6000的結果
 	err = globalDB.C("bank").UpdateId(entry.ID, &entry)
 
 	if err != nil {
