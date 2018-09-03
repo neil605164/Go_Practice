4a826d28f807b1731a974482c5886ff74aaf92a7
neil605164 <neil605164work@gmail.com>
2018-09-03 16:51:41 +0800

    [ADD] Prac_channel AND gproutine

--- main.go
+++ main.go
@@ -17,9 +17,16 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
+// 全域變數
 var globalDB *mgo.Database
 var account = "neil_hsieh"
-var mu = &sync.Mutex{}
+var input chan string
+var output chan result
+
+type result struct {
+	Account string
+	Result  float64
+}
 
 type currency struct {
 	ID      bson.ObjectId `json:"id" bson:"_id,omitempty"`
@@ -36,43 +43,31 @@ func Random(min, max int) int {
 }
 
 func pay(w http.ResponseWriter, r *http.Request) {
-	entry := currency{}
+	wg := sync.WaitGroup{}
+	wg.Add(1)
 
-	// 查看mu記憶體位址（0x874958）
-	fmt.Printf("mu in pay func memory loction is %p", mu)
+	go func(wg *sync.WaitGroup) {
+		input <- account
+		for {
+			select {
+			case data := <-output:
+				fmt.Printf("%v\n", data)
+				wg.Done()
+				return
+			}
+		}
+	}(&wg)
 
-	//將以下程式碼lock住，不讓其他請求先行執行
-	mu.Lock()
-	// 當執行完畢pay handler最後一行後，會在執行defer底下的條件
-	// 在此是unlock程式碼
-	defer mu.Unlock()
-
-	// step 1: get current amount
-	err := globalDB.C("bank").Find(bson.M{"account": account}).One(&entry)
-
-	if err != nil {
-		panic(err)
-	}
-
-	wait := Random(1, 100)
-	time.Sleep(time.Duration(wait) * time.Millisecond)
-
-	//step 3: subtract current balance and update back to database
-	entry.Amount = entry.Amount + 50.000 // 預計取的6000的結果
-	err = globalDB.C("bank").UpdateId(entry.ID, &entry)
-
-	if err != nil {
-		panic("update error")
-	}
-
-	fmt.Printf("%+v\n", entry)
-
+	wg.Wait()
 	io.WriteString(w, "ok")
 }
 
 func main() {
-	// 查看mu記憶體位址（0x874958）
-	fmt.Printf("mu memory loction is %p", mu)
+	// 宣告兩個channel
+	// input 型態為 string
+	// output 型態為 result （struct）
+	input = make(chan string)
+	output = make(chan result)
 
 	// 建立連線
 	session, err := mgo.Dial("localhost:8082")
@@ -90,6 +85,37 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	// 建立一個goroutine，並將input丟入
+	// 若input有值，開始執行case內的任務
+	go func(input *chan string) {
+		for {
+			select {
+			case account := <-*input:
+				entry := currency{}
+
+				// step 1: get current amount
+				err := globalDB.C("bank").Find(bson.M{"account": account}).One(&entry)
+
+				if err != nil {
+					panic(err)
+				}
+
+				//step 3: subtract current balance and update back to database
+				entry.Amount = entry.Amount + 50.000 // 預計取的6000的結果
+				err = globalDB.C("bank").UpdateId(entry.ID, &entry)
+
+				if err != nil {
+					panic("update error")
+				}
+
+				output <- result{
+					Account: account,
+					Result:  entry.Amount,
+				}
+			}
+		}
+	}(&input)
 
 	log.Println("Listen server on 8000 port")
 	http.HandleFunc("/", pay)
