81af781fc27c83daceace3673df409f91e7db598
neil_hsieh <rneil0427@cchntek.com>
2020-04-17 16:14:01 +0800

    [add] sync pool prac

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+
+		p := &sync.Pool{
+			New: func() interface{} {
+				return 0
+			},
+		}
+
+		a := p.Get().(int)
+
+		fmt.Println("A ==>", a) // 0
+		p.Put(1)
+		runtime.GC()
+
+		// time.Sleep(time.Second * 1)
+		b := p.Get().(int)
+		fmt.Println("A ==>", a) // 0
+		fmt.Println("B ==>", b) // 1
+
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":8787") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
