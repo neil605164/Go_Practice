a5242777eb58dbc23c45f4404c092037c86f587d
neil_hsieh <rneil0427@cchntek.com>
2020-04-20 10:25:14 +0800

    [add] singleflight ==> 用以處理同分秒呼叫其他單位 api 可以減少呼叫的次數

--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/sync/singleflight"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.New()
+
+	var singleFlight singleflight.Group
+
+	r.GET("/ping", func(c *gin.Context) {
+
+		v, err, shared := singleFlight.Do("github", func() (interface{}, error) {
+			return githubStatus()
+		})
+		if err != nil {
+			fmt.Println("Error Msg ===>", err)
+			return
+		}
+
+		status := v.(string)
+
+		c.JSON(200, gin.H{
+			"status": status,
+			"shared": shared,
+		})
+	})
+	r.Run(":8787")
+}
+
+func githubStatus() (string, error) {
+	log.Println("Make Request to Github API")
+	defer log.Println("Request to GitHub API Complete")
+
+	resp, err := http.Get("https://status.github.com/api/status.json")
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return "", fmt.Errorf("github response: %s", resp.Status)
+	}
+
+	status := strconv.Itoa(resp.StatusCode)
+
+	return status, err
 }
