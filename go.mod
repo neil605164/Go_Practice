43f4157fff74e0ef9d1534c4798c47eda165891d
neil_hsieh <rneil0427@cchntek.com>
2020-05-04 17:01:21 +0800

    [add] prac import self pkg

---
+++ go.mod
@@ -0,0 +1,8 @@
+module Go_Practice
+
+go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.6.2
+	github.com/neil605164/go_pkg v0.0.1
+)
--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	mathtest "github.com/neil605164/go_pkg"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+
+		floatList := []float64{2.2, 3.2, 5.5}
+
+		fmt.Println(floatList)
+
+		result := mathtest.Average(floatList)
+		fmt.Println(result)
+
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run(":1212")
 }
