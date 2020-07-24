b0bdf0549858fe01cbb581f669a079c01005e943
neil_hsieh <rneil0427@cchntek.com>
2020-07-24 11:35:03 +0800

    leetcode running sum of 1d array

--- main.go
+++ main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	nums := []int{1, 2, 3, 4}
+	res := runningSum(nums)
+
+	fmt.Println(res)
+}
+
+func runningSum(nums []int) []int {
+
+	for i := 1; i < len(nums); i++ {
+		nums[i] += nums[i-1]
+	}
+	return nums
 }
