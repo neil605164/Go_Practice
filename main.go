b86fd7808e6d214e8175d6b99ed30531c4250010
neil_hsieh <rneil0427@cchntek.com>
2020-07-24 10:43:21 +0800

    [add] two sum prac

--- main.go
+++ main.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	nums := []int{2, 7, 11, 15}
+	target := 26
+
+	res := twoSum(nums, target)
+	fmt.Println(res)
+}
+
+func twoSum(nums []int, target int) []int {
+
+	m := make(map[int]int)
+	for i, n := range nums {
+		if j, ok := m[target-n]; ok {
+			return []int{j, i}
+		}
+		m[n] = i
+	}
+	return []int{}
 }
