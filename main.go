d8f43376c8306fb2fb9e157598d8d7f79cf581f7
Neil <neil@NeildeMacBook-Pro.local>
2020-08-02 23:02:11 +0800

    [add] leetcode count good triplets

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	arrs := []int{1, 1, 2, 2, 3}
+	res := countGoodTriplets(arrs, 0, 0, 1)
+
+	fmt.Println(res)
+}
+
+func countGoodTriplets(arr []int, a int, b int, c int) int {
+	sum := 0
+
+	var i, j, k int
+	for i = range arr {
+		for j = len(arr) - 1; j > i; j-- {
+			for k = len(arr) - 1; k > j; k-- {
+
+				if withBranch(arr[i]-arr[j]) > a {
+					continue
+				}
+
+				if withBranch(arr[j]-arr[k]) > b {
+					continue
+				}
+
+				if withBranch(arr[i]-arr[k]) > c {
+					continue
+				}
+
+				res := fmt.Sprintf("I=%d, J=%d, K=%d", i, j, k)
+				fmt.Println(res)
+				sum++
+			}
+		}
+	}
+
+	return sum
+}
+
+func withBranch(n int) int {
+	if n < 0 {
+		return -n
+	}
+
+	return n
 }
