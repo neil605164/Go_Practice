2584bc5d5025843a7eb3a2afc17e61f52bbc3c02
neil_hsieh <rneil0427@cchntek.com>
2020-08-03 17:33:54 +0800

    [add] another answers

--- main.go
+++ main.go
@@ -3,62 +3,35 @@ package main
 import "fmt"
 
 func main() {
-	// nums := []int{9}
+	nums := []int{9}
 	// nums := []int{8, 9}
 	// nums := []int{9, 9}
 	// nums := []int{8, 9, 9}
 	// nums := []int{1, 2, 3}
-	nums := []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+	// nums := []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
 	res := plusOne(nums)
 	fmt.Println(res)
 }
 
 func plusOne(digits []int) []int {
-	l := len(digits)
-	var tmp, res []int
-
-	mustPlus := false
-	for i := l - 1; i >= 0; i-- {
-		// 如果是最後一格
-		if i == l-1 {
-			if digits[i]+1 >= 10 {
-				tmp = append(tmp, 0)
-
-				// 長度只有 1 才需要
-				if l == 1 {
-					tmp = append(tmp, 1)
+	carry := true
+	for i := len(digits) - 1; i >= 0; i-- {
+		if carry {
+			if digits[i] == 9 {
+				digits[i] = 0
+				if i == 0 {
+					digits = append(digits, 0)
+					digits[0] = 1
 				}
-
-				mustPlus = true
+				carry = true
 			} else {
-				tmp = append(tmp, digits[i]+1)
-				mustPlus = false
+				digits[i] += 1
+				carry = false
 			}
 		} else {
-			if mustPlus {
-				if digits[i]+1 >= 10 {
-					tmp = append(tmp, 0)
-
-					if i == 0 {
-						tmp = append(tmp, 1)
-					}
-
-					mustPlus = true
-				} else {
-					tmp = append(tmp, digits[i]+1)
-					mustPlus = false
-				}
-			} else {
-				tmp = append(tmp, digits[i])
-				mustPlus = false
-			}
+			return digits
 		}
 	}
 
-	// 顛倒順序
-	for j := len(tmp) - 1; j >= 0; j-- {
-		res = append(res, tmp[j])
-	}
-
-	return res
+	return digits
 }
