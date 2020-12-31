d672c77053781055fe66808857b05cd1ddc570c0
neil_hsieh <rneil0427@cchntek.com>
2020-12-31 16:34:33 +0800

    [add]prac interface

--- main.go
+++ main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+type IPv4 []byte
+
+func (i IPv4) String() string {
+	return fmt.Sprintf("%v.%v.%v.%v", i[0], i[1], i[2], i[3])
+}
+
 func main() {
-	fmt.Println("Hello World")
+
+	ipv4 := map[string]IPv4{
+		"localhost": {127, 0, 0, 1},
+		"google":    {8, 8, 8, 8},
+	}
+
+	for k, v := range ipv4 {
+		fmt.Printf("%v: %v\n", k, v)
+	}
 }
