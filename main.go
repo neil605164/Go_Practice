75389501ba26af88185db7c3bac85178a9661788
rneil0427 <rneil0427@ny.corp>
2022-04-25 14:35:11 +0800

    [add]prac leetcode

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	x := 10
+
+	fmt.Println(isPalindrome(x))
+
+}
+
+func isPalindrome(x int) bool {
+
+	if x < 0 {
+		return false
+	}
+
+	if x > int(math.Pow(2, 31)-1) {
+		return false
+	}
+
+	var intL []int
+	tmp := x
+	for tmp != 0 {
+		intL = append(intL, tmp%10)
+		tmp /= 10
+	}
+
+	time := 1
+	tmp = 0
+	for i := len(intL) - 1; i >= 0; i-- {
+		tmp += intL[i] * time
+		time *= 10
+	}
+
+	return x == tmp
 }
