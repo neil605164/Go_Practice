6e442933bf4ab28a7c21b2e4b218bfc5a2c0c763
rneil0427 <rneil0427@ny.corp>
2023-05-30 23:33:25 +0800

    [fix] 修改 conf

--- main.go
+++ main.go
@@ -8,14 +8,14 @@ import (
 
 func main() {
 	// /depts
-	sub := "neil"         // 想要访问资源的用户。
-	company := "companyc" // 将被访问的资源。
-	org := "orgc"
-	depart := "departc"
+	// 想要访问资源的用户。
+	company := "companya" // 将被访问的资源。
+	org := "orga"
+	depart := "departa"
 	act := "GetUserList" // 用户对资源执行的操作。
 	e, _ := casbin.NewEnforcer("resources/model.conf", "resources/p.csv")
 
-	ok, _ := e.Enforce(sub, company, org, depart, act)
+	ok, _ := e.Enforce(depart, company, org, act)
 	if ok {
 		log.Println("运行通过")
 	} else {
