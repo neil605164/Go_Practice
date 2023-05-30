339c8d02c9f02f045de21c44acb32c339df93c2c
rneil0427 <rneil0427@ny.corp>
2023-05-30 20:22:47 +0800

    [fix] casbin

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/casbin/casbin/v2"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// /depts
+	sub := "neil"         // 想要访问资源的用户。
+	company := "companyc" // 将被访问的资源。
+	org := "orgc"
+	depart := "departc"
+	act := "GetUserList" // 用户对资源执行的操作。
+	e, _ := casbin.NewEnforcer("resources/model.conf", "resources/p.csv")
+
+	ok, _ := e.Enforce(sub, company, org, depart, act)
+	if ok {
+		log.Println("运行通过")
+	} else {
+		log.Println("运行不通过")
+	}
+
 }
