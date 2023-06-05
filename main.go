b3422ca4bceeb09323cf3de5f1765d9469b04938
rneil0427 <rneil0427@ny.corp>
2023-06-05 22:58:27 +0800

    [fix] 調整 conf 寫法

--- main.go
+++ main.go
@@ -7,15 +7,24 @@ import (
 )
 
 func main() {
-	// /depts
 	// 想要访问资源的用户。
+	role := "posa"        // 角色名称，根据角色策略文件选择合适的角色。
 	company := "companya" // 将被访问的资源。
 	org := "orga"
-	depart := "departa"
 	act := "GetUserList" // 用户对资源执行的操作。
-	e, _ := casbin.NewEnforcer("resources/model.conf", "resources/p.csv")
 
-	ok, _ := e.Enforce(depart, company, org, act)
+	e, err := casbin.NewEnforcer("resources/model.conf", "resources/p.csv")
+	if err != nil {
+		panic(err)
+	}
+	// Enable debugging
+	e.EnableLog(true)
+
+	ok, err := e.Enforce(role, org, company, act)
+	if err != nil {
+		panic(err)
+	}
+
 	if ok {
 		log.Println("运行通过")
 	} else {
