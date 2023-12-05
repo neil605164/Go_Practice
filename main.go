6ca5558c5a846f6ef2028454f2b6a3c514b59a35
neil <neil.hsieh@innotech.me>
2023-12-05 15:23:59 +0800

    [add] uuid example

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println(uuid.New().String())
+	fmt.Println(uuid.New().String())
+	fmt.Println(uuid.New().String())
 }
